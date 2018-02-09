b0937f3925a23f7030567322206728c35b58ea7f
Victor Evangelista <victorevangelista@gmail.com>
2018-02-09 14:22:27 -0200

    Backend - Ciclo de Pagamento API: Registrando as Rotas do Serviço de Ciclo de Pagamentos

---
+++ backend/config/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+module.exports = function(server) {
+	//API de Rotas
+	const router = express.Router()
+	server.use('/api', router)
+
+	// rotas da API
+	const billingCycleService = require('../api/biilingCycle/billingCycleService')
+	billingCycleService.register(router, '/biilingCycles')
+}
