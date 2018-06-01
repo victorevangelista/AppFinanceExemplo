fdcad883dccc269311b2e768d7d4f517373b9545
Victor Evangelista <victorevangelista@gmail.com>
2018-06-01 15:48:38 -0300

    Frontend - Estrutura Básica do Projeto (Template): Lista de Ciclo de Pagamentos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -7,15 +7,24 @@
 
 	function BillingCycleController($http, msgs){
 		const vm = this
+		const url = 'http://localhost:3003/api/billingCycles'
+
+		vm.refresh = function() {
+			$http.get(url).then(function(response){
+				vm.billingCycle = {}
+				vm.billingCycles = response.data
+			})
+		}
 
 		vm.create = function() {
-			const url = 'http://localhost:3003/api/billingCycles'
 			$http.post(url, vm.billingCycle).then(function(response){
-				vm.billingCycle = {}
+				vm.refresh()
 				msgs.addSuccess('Operação realizada com sucesso!')
 			}).catch(function(response){
 				msgs.addError(response.data.errors)
 			})
 		}
+
+		vm.refresh()
 	}
 })()
