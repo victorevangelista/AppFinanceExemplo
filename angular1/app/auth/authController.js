11f136ba10c0c31b17dbe5caa9e4dba6b1edb878
Victor Evangelista <victorevangelista@gmail.com>
2018-06-22 19:09:04 -0300

    Frontend - Autenticação: Estrutura Básica do Login/Cadastro

--- angular1/app/auth/authController.js
+++ angular1/app/auth/authController.js
@@ -8,6 +8,18 @@
 	function AuthController($location, msgs){
 		const vm = this
 
+		vm.loginMode = true
+
+		vm.changeMode = () => vm.loginMode = !vm.loginMode
+
+		vm.login = () => {
+			console.log(`Login... ${vm.user.email}`)
+		}
+
+		vm.signup = () => {
+			console.log(`Signup... ${vm.user.email}`)	
+		}
+
 		vm.getUser = () => ({name: 'Victor Evangelista', email: 'meuemail@gmail.com'})
 
 		vm.logout = () => {
